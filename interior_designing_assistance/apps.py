850aa02087668d4592b924c8dd26e3c8492892fa
jimson js <jimsonjs7@gmail.com>
2024-12-09 22:13:34 +0530

    first commit

---
+++ interior_designing_assistance/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class InteriorDesigningAssistanceConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'interior_designing_assistance'
