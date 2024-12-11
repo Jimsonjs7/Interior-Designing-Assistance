95d2498484b975d3d1dbf8a5d84042501469fca7
jimson js <jimsonjs7@gmail.com>
2024-12-12 00:43:45 +0530

    second commit

--- interior_designing_assistance/views.py
+++ interior_designing_assistance/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+def home(request):
+    return render(request,'home.html')
+"""
+def contact(request):
+    return render(request, 'dashboard/contact.html')
 
-# Create your views here.
+def dashboard(request):
+    return render(request, 'dashboard/dashboard.html')
+
+def about(request):
+    return render(request, 'dashboard/about.html')
+
+"""
